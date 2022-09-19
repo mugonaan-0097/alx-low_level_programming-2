df6cac657bd9afe24a57638db8ee6bed796acc26
mugonaan-0097 <mugonaan@gmail.com>
2022-09-19 10:54:11 -0700

    smash

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -4,7 +4,7 @@
  * reset_to_98 - pointer to an int as parameter and
  * updates the value it points to to 98.
  * @n: n is the pointer of n and return value 98.
- * Return: Always 98.
+ * Return: void
  */
 void reset_to_98(int *n)
 {
