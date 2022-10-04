49f0f70812a85daa7db2819364fdf6e68d6cb246
mugonaan-0097 <mugonaan@gmail.com>
2022-10-04 12:55:12 -0700

    gra

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_last_digit - the function.
+ *
+ * @er: is the variable of the function.
+ * Return: Always 0.
+ */
+int print_last_digit(int r)
+{
+	int num;
+
+	num = r % 10;
+	if (num < 0)
+	{
+		num = num * -1;
+	}
+	_putchar(num + '0');
+	return (num);
+}
