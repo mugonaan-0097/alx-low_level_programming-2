997ce282d7d9d9e14aad949f7c40f86f11530138
mugonaan-0097 <mugonaan@gmail.com>
2022-10-04 14:49:18 -0700

    sugar

---
+++ 0x09-static_libraries/4-isalpha.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isalpha - Entry point
+ *
+ * @c: c is the variable for the program
+ * Return: Always 0.
+ */
+int _isalpha(int c)
+{
+	if (('z' > c && c > 'a') || ('Z' > c && c > 'A'))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
---
+++ 0x09-static_libraries/main.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * main - my main.c file
+ * Return:Always 0
+ */
+int main(void)
+{
+	_puts("\"At the end of the day. my goal was to be the best hacker\"\n\t- Danladi Daloek");
+	return (0);
+}
